a23bfb15c59e80ddc6233c9252d628e0b76ed99a
Vivek Singh <petmongrels@gmail.com>
2021-06-02 13:02:57 +0530

    .

---
+++ scripts/mrs-patch.sh
@@ -0,0 +1,1 @@
+scp /Users/vsingh/Projects/samanvay/bahmni/openmrs-module-webservices.rest/omod-1.8/target/classes/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/ChangePasswordController1_8.class /opt/openmrs/.openmrs-lib-cache/bahmni.ie.apps/org/openmrs/module/webservices/rest/web/v1_0/controller/openmrs1_8/ChangePasswordController1_8.class
